f1e5825d15b0ea3dc487ce06eb2f44d2f0219014
Krutie Thakkar <Krutie@gmail.com>
2016-09-19 13:14:35 -0500

    final ceasar

--- main.py
+++ main.py
@@ -1,25 +1,57 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-import webapp2
 
-class MainHandler(webapp2.RequestHandler):
+import webapp2
+import cgi
+
+
+from ceasar import encrypt
+form="""
+<form method="post">
+    <label> What text do you want to encrypt?<input name="thingy" value="%(thingy)s">
+    </label>
+    <br>
+    <br>
+    <label> How many numbers do you want to encrypt by?<input name="rot" value="%(rot)s">
+    </label>
+    <div style="color: red">%(error)s</div>
+    <br>
+    <br>
+    <input type="submit" value="Submit"/>
+</form>
+"""
+
+#how to validate negative numbers and 0? isinstance
+def valid_rot(rot):
+    if rot and rot.isdigit():
+        return int(rot)
+#def escape_html(s):
+#    return cgi.escape(s, quote =True)
+
+
+class AddThing(webapp2.RequestHandler):
+    """requests to encrypt"""
+    def write_form(self, error="", rot="", thingy=""):
+        self.response.out.write(form % {"error": (error),
+                                        "rot": (rot),
+                                        "thingy":(thingy)})
+
     def get(self):
-        self.response.write('Hello world!')
+        self.write_form()
+
+    def post(self):
+
+        user_rot = self.request.get('rot')
+        thingy = self.request.get('thingy')
+        rot = valid_rot(user_rot)
+        encrypted_word = encrypt(thingy, rot)
+
+        if not rot:
+            self.write_form("That is not a valid rotation number.", user_rot, thingy)
+        else:
+            self.write_form("", "", encrypted_word)
+
+
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', AddThing)
 ], debug=True)
